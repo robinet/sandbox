70d0838ae827c7eacc6590f2b5e7868a2f78da59
Cocho <agrobinet@gmail.com>
2022-09-05 00:46:03 -0300

    Add full folder structure generation to Get-TerminalGitHubCommandsFromFolder

--- tools/windows-terminal/Get-TerminalGitHubCommandsFromFolder.ps1
+++ tools/windows-terminal/Get-TerminalGitHubCommandsFromFolder.ps1
@@ -1,7 +1,7 @@
 <#
 .EXAMPLE
 
-./Get-TerminalGitHubCommandsFromFolder.ps1 -Folder '<path/to/parent-repos-folder>' -TabColor '#FC4C02A'  -Dark | ConvertTo-Json | Out-File <terminal-commands.json>
+./Get-TerminalGitHubCommandsFromFolder.ps1 -Folder '<path/to/parent-repos-folder>' -TabColor '#FC4C02A' -Dark | ConvertTo-Json | Out-File <terminal-commands.json>
 #>
 
 param (
@@ -10,15 +10,16 @@ param (
   [switch] $Dark
 )
 
-$Folder = Resolve-Path $Folder
+$homeFolder = (Get-Item -Path (Resolve-Path $Folder).Path)
 
-return (Get-ChildItem -Path $Folder -Directory).Name `
+# Sub folders
+$commands = (Get-ChildItem -Path $homeFolder.FullName -Directory).Name `
 | ForEach-Object {
   $name = $_
 
   $command = @{
     action                   = 'newTab'
-    startingDirectory        = (Join-Path -Path $Folder -ChildPath $name).Replace('\', '/')
+    startingDirectory        = (Join-Path -Path $homeFolder.FullName -ChildPath $name).Replace('\', '/')
     suppressApplicationTitle = $true
     tabTitle                 = $name
   }
@@ -32,27 +33,69 @@ return (Get-ChildItem -Path $Folder -Directory).Name `
   $icon = $Dark.IsPresent ? "$icon-dark.png" : "$icon.png"
 
   return [pscustomobject] @{
-    action  = 'newTab';
-    icon    = $icon;
-    name    = $name;
+    action  = 'newTab'
+    icon    = $icon
+    name    = $name
     command = [pscustomobject]$command
   }
+}
+
+# Home folder
+$homeCommand = @{
+  action                   = 'newTab'
+  startingDirectory        = $homeFolder.FullName.Replace('\', '/')
+  suppressApplicationTitle = $true
+  tabTitle                 = $homeFolder.Name
+}
+if (-not [string]::IsNullOrWhiteSpace($TabColor)) {
+  $homeCommand.Add('tabColor', $TabColor)
+}
+$commands = @([pscustomobject]@{
+    action  = 'newTab'
+    icon    = '▶️'
+    name    = "//$($homeFolder.Name)"
+    command = [pscustomobject]$homeCommand
+  }) `
+  + $commands
+
+return [pscustomobject]@{
+  name     = "/$($homeFolder.Name)"
+  icon     = '📂'
+  commands = $commands
 }
 
 
 
 <#
 {
-  "action": "newTab",
-  "command": 
-  {
+  "name": "/<organization>",
+  "icon": "📂",
+  "commands": [
+    {
       "action": "newTab",
-      "startingDirectory": "C:/Projects/GitHub/<organization>/ic2-iac",
-      "suppressApplicationTitle": true,
-      "tabTitle": "ic2-iac",
-      "tabColor": "#06332A"
-  },
-  "icon": "https://github.githubassets.com/favicons/favicon-dark.png", // /favicons/favicon[-dark].png
-  "name": "ic2-iac"
+      "icon": "▶️",
+      "name": "//<organization>",
+      "command": {
+        "action": "newTab",
+        "tabTitle": "<organization>",
+        "tabColor": "#CCCCCC",
+        "suppressApplicationTitle": true,
+        "startingDirectory": "C:/Projects/GitHub/<organization>"
+      }
+    },
+    {
+      "action": "newTab",
+      "icon": "https://github.githubassets.com/favicons/favicon-dark.png", // /favicons/favicon[-dark].png
+      "name": "ic2-iac",
+      "command": 
+      {
+          "action": "newTab",
+          "startingDirectory": "C:/Projects/GitHub/<organization>/ic2-iac",
+          "suppressApplicationTitle": true,
+          "tabTitle": "ic2-iac",
+          "tabColor": "#06332A"
+      }
+    }
+  ]
 }
 #>
